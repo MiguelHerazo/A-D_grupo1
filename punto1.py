1cc9df954826133aef98e1a63475badf6402d794
julianabetancurb <110195024+julianabetancurb@users.noreply.github.com>
2024-02-25 19:51:48 -0500

    Update punto1.py

--- punto1.py
+++ punto1.py
@@ -1,38 +1,43 @@
-def imprimir_en_caracol_inverso(matriz):
-    if not matriz:
+#Dado la dimensión de una matriz cuadrada muestre los
+#valores en forma de caracol o vórtice.
+def caracol(matriz):
+    if not matriz or not matriz[0]: # O(1)
         return
 
-    filas, columnas = len(matriz), len(matriz[0])
-    inicio_fila, inicio_columna = 0, 0
-    fin_fila, fin_columna = filas - 1, columnas - 1
+    n = len(matriz) # O(1)
+    inicio_fila, inicio_columna = 0, 0 # O(1)
+    fin_fila, fin_columna = n - 1, n - 1 # O(1)
 
-    while inicio_fila <= fin_fila and inicio_columna <= fin_columna:
-        for i in range(inicio_fila, fin_fila + 1):
-            print(matriz[i][inicio_columna], end=" ")
+    while inicio_fila <= fin_fila and inicio_columna <= fin_columna: # O(n)
 
-        inicio_columna += 1
+        for i in range(inicio_fila, fin_fila + 1):  # O(n)
+            print(matriz[i][inicio_columna], end=" ")  # O(n)
 
-        for i in range(inicio_columna, fin_columna + 1):
-            print(matriz[fin_fila][i], end=" ")
+        inicio_columna += 1  # O(1)
 
-        fin_fila -= 1
+        for i in range(inicio_columna, fin_columna + 1):  # O(n)
+            print(matriz[fin_fila][i], end=" ")  # O(n)
 
-        if inicio_columna <= fin_columna:
-            for i in range(fin_fila, inicio_fila - 1, -1):
-                print(matriz[i][fin_columna], end=" ")
+        fin_fila -= 1  # O(1)
 
-            fin_columna -= 1
+        for i in range(fin_fila, inicio_fila - 1, -1):  # O(n)
+            print(matriz[i][fin_columna], end=" ")  # O(n)
 
-        if inicio_fila <= fin_fila:
-            for i in range(fin_columna, inicio_columna - 1, -1):
-                print(matriz[inicio_fila][i], end=" ")
+        fin_columna -= 1  # O(1)
 
-            inicio_fila += 1
+        for i in range(fin_columna, inicio_columna - 1, -1):  # O(n)
+            print(matriz[inicio_fila][i], end=" ")  # O(n)
 
+        inicio_fila += 1  # O(1)
+
+# Ejemplo de uso con la matriz dada
 matriz = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
 
-imprimir_en_caracol_inverso(matriz)
+caracol(matriz)
+
+#BigO =  # O(n^2)
+#Complejidad espacial: O(n^2) 
