fe2e1b59986d54455ec26d4194e318fc585444fa
julianabetancurb <110195024+julianabetancurb@users.noreply.github.com>
2024-03-17 17:07:57 -0500

    Update PrincipioConteo.py

--- PrincipioConteo.py
+++ PrincipioConteo.py
@@ -1,21 +1,22 @@
-def principio_conteo_aditivo(m, n):
-    return m + n
+def principio_conteo_aditivo(m, n):  # O(1)
+    return m + n  # O(1)
 
-def principio_conteo_multiplicativo(m, n):
-    return m * n
+def principio_conteo_multiplicativo(m, n):  # O(1)
+    return m * n  # O(1)
 
-def main():
-    print("Principio de conteo aditivo:")
-    m_aditivo = int(input("Ingrese el número de formas para el primer evento (m): "))
-    n_aditivo = int(input("Ingrese el número de formas para el segundo evento (n): "))
-    resultado_aditivo = principio_conteo_aditivo(m_aditivo, n_aditivo)
-    print(f"El número total de formas en que ambos eventos pueden ocurrir es: {resultado_aditivo}")
+def main():  # O(1)
+    print("Principio de conteo aditivo:")  # O(1)
+    m_aditivo = int(input("Ingrese el número de formas para el primer evento (m): "))  # O(1)
+    n_aditivo = int(input("Ingrese el número de formas para el segundo evento (n): "))  # O(1)
+    resultado_aditivo = principio_conteo_aditivo(m_aditivo, n_aditivo)  # O(1)
+    print(f"El número total de formas en que ambos eventos pueden ocurrir es: {resultado_aditivo}")  # O(1)
 
-    print("\nPrincipio de conteo multiplicativo:")
-    m_multiplicativo = int(input("Ingrese el número de formas para el primer evento (m): "))
-    n_multiplicativo = int(input("Ingrese el número de formas para el segundo evento (n): "))
-    resultado_multiplicativo = principio_conteo_multiplicativo(m_multiplicativo, n_multiplicativo)
-    print(f"El número total de formas en que ambos eventos pueden ocurrir es: {resultado_multiplicativo}")
+    print("\nPrincipio de conteo multiplicativo:")  # O(1)
+    m_multiplicativo = int(input("Ingrese el número de formas para el primer evento (m): "))  # O(1)
+    n_multiplicativo = int(input("Ingrese el número de formas para el segundo evento (n): "))  # O(1)
+    resultado_multiplicativo = principio_conteo_multiplicativo(m_multiplicativo, n_multiplicativo)  # O(1)
+    print(f"El número total de formas en que ambos eventos pueden ocurrir es: {resultado_multiplicativo}")  # O(1)
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":  # O(1)
+    main()  # O(1)
+
