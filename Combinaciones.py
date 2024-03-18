3a41d7c7e282c5c3c134bf39873d9d6681a5ca1d
julianabetancurb <110195024+julianabetancurb@users.noreply.github.com>
2024-03-17 22:47:28 -0500

    Update Combinaciones.py

--- Combinaciones.py
+++ Combinaciones.py
@@ -2,8 +2,8 @@ import math
 
 # Función para calcular combinaciones con repetición
 def combinaciones_con_repeticion(m, n):
-    return math.comb(m + n - 1, n)
+    return math.comb(m + n - 1, n)#O(n)
 
 # Función para calcular combinaciones sin repetición
 def combinaciones_sin_repeticion(m, n):
-    return math.comb(m, n)
+    return math.comb(m, n)#O(n)
