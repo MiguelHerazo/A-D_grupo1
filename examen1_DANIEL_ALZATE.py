fd52b6a6f9314a3108b8e66c21f9df6a4e068f26
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2024-04-12 15:11:26 -0500

    Update and rename examen1.py to examen1_DANIEL_ALZATE.py
    Daniel Alzate Arias exam 1

--- examen1_DANIEL_ALZATE.py
+++ examen1_DANIEL_ALZATE.py
@@ -4,7 +4,10 @@
 
 # PUNTO 1
 
+#Punto 1 (1.5): Resolver la siguiente relación de recurrencia entregando la recurrencia sin recursividad y su ecuación particular según las condiciones de frontera:n>=4 donde, 𝑎0 = # 𝑑𝑒 𝑙𝑒𝑡𝑟𝑎𝑠 𝑑𝑒 𝑠𝑢 𝑝𝑟𝑖𝑚𝑒𝑟 𝑛𝑜𝑚𝑏𝑟𝑒 y 𝑎1 = # 𝑑𝑒 𝑙𝑒𝑡𝑟𝑎𝑠 𝑑𝑒 𝑠𝑢 𝑝𝑟𝑖𝑚𝑒𝑟 𝑎𝑝𝑒𝑙𝑙𝑖𝑑𝑜 hallar el termino 1500 de la sucesión.
 
+
+ #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 # PUNTO 2
@@ -27,6 +30,10 @@ limite = 20
 multiplos = encontrar_multiplos(numero, limite)
 
 print(f"Los múltiplos de {numero} hasta {limite} son: {multiplos}")
+
+
+
+ #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 #PUNTO 3
