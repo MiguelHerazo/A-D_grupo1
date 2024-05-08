a703f7ff50cfc79ee9a495cbfcb39c79c6c13dbb
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2024-05-08 15:04:05 -0500

    Update Punto1.py

---
+++ TallerDiseñoDeAlgoritmos/Punto1.py
@@ -0,0 +1,25 @@
+#Dada una matriz n*n mostrar el camino más óptimo desde la posición (0,0) a (n,n).
+
+def greedy_path(matrix, x, y):
+    if x == len(matrix) - 1 and y == len(matrix[0]) - 1:
+        return [(x, y)]
+    
+    if x == len(matrix) - 1:
+        return [(x, y)] + greedy_path(matrix, x, y + 1)
+    
+    if y == len(matrix[0]) - 1:
+        return [(x, y)] + greedy_path(matrix, x + 1, y)
+    
+    if matrix[x+1][y] < matrix[x][y+1]:
+        return [(x, y)] + greedy_path(matrix, x + 1, y)
+    else:
+        return [(x, y)] + greedy_path(matrix, x, y + 1)
+
+matrix = [
+    [1, 3, 1],
+    [1, 9, 1],
+    [4, 2, 1]
+]
+
+path = greedy_path(matrix, 0, 0)
+print("Camino_Optimo:",path)
