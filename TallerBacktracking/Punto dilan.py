81fd89d48de9bd80e312d1d87e50ce5c258b6fbd
Dilan07 <91625566+dilan071@users.noreply.github.com>
2024-05-15 15:55:37 -0500

    Update Punto dilan.py

--- TallerBacktracking/Punto dilan.py
+++ TallerBacktracking/Punto dilan.py
@@ -4,37 +4,35 @@
 # manera que la diferencia absoluta entre sus 
 # sumas sea mínima y encontrar la diferencia mínima
 
-class Solution {
-public:
-    int minDifference(int arr[], int n)  {
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += arr[i];
-        }
+class Solution:
+    def minDifference(self, arr, n):
+        total_sum = sum(arr)
+        dp = [[False] * (total_sum + 1) for _ in range(n + 1)]
         
-        bool dp[n + 1][sum + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
-        }
+        for i in range(n + 1):
+            dp[i][0] = True
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (arr[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
+        for i in range(1, n + 1):
+            for j in range(1, total_sum + 1):
+                if arr[i - 1] <= j:
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
+                else:
+                    dp[i][j] = dp[i - 1][j]
         
-        int minDiff = INT_MAX;
-        for (int j = sum / 2; j >= 0; j--) {
-            if (dp[n][j]) {
-                minDiff = min(minDiff, sum - 2 * j);
-                break;
-            }
-        }
+        min_diff = float('inf')
+        for j in range(total_sum // 2, -1, -1):
+            if dp[n][j]:
+                min_diff = min(min_diff, total_sum - 2 * j)
+                break
         
-        return minDiff;
-    }
-};
+        return min_diff
+
+# Ejemplo de uso
+sol = Solution()
+arr1 = [1, 6, 11, 5]
+n1 = len(arr1)
+print("Diferencia mínima:", sol.minDifference(arr1, n1))
+
+arr2 = [1, 4]
+n2 = len(arr2)
+print("Diferencia mínima:", sol.minDifference(arr2, n2))
